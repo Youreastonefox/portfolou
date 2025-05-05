28b67f7dc8849f7831223737c56484e9b2ff2168
Youreastonefox <louisa.chd@gmail.com>
2025-05-05 19:51:54 +0200

    Initial commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <!-- <div class="home__main"> -->
+    <p class="home__name">Louisa Chaduc</p>
+    <p class="home__title">Software Engineer</p>
+    <!-- </div> -->
+  </div>
+</template>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  height: 100vh;
+  width: 100%;
+}
+</style>
