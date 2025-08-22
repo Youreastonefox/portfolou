812ad11a666c0ce440e2776c329ec7b7dcadd97a
Youreastonefox <louisa.chd@gmail.com>
2025-08-22 18:54:15 +0200

    ⚙️ chore | Setup Sass files

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import App from './App.vue'
+
+import '@/style/main.scss'
+
 import { createPinia } from 'pinia'
+
+import router from './router.ts'
+
 import { createI18n } from 'vue-i18n'
 import en from '@/translations/en.json'
 import fr from '@/translations/fr.json'
 
-import App from './App.vue'
-import router from './router.ts'
-
 const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
 
 const i18n = createI18n({
   locale: navigator.language,
@@ -18,8 +23,5 @@ const i18n = createI18n({
   legacy: false,
 })
 app.use(i18n)
-
-app.use(createPinia())
-app.use(router)
 
 app.mount('#app')
