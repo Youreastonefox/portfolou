4ed41c4f7615bb80f4ec80164817eb8825f0f8b4
Youreastonefox <louisa.chd@gmail.com>
2025-08-22 18:46:52 +0200

    ⚙️ chore | Setup i18nplugin  andtranslation component

---
+++ src/components/common/TranslationComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="changeLocale">
+    {{ currentLocale === 'en' ? '🇫🇷' : '🇬🇧' }}
+  </button>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+const { locale } = useI18n()
+
+const currentLocale = locale
+
+function changeLocale() {
+  locale.value = locale.value === 'en' ? 'fr' : 'en'
+}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
+import en from '@/translations/en.json'
+import fr from '@/translations/fr.json'
 
 import App from './App.vue'
 import router from './router.ts'
 
 const app = createApp(App)
+
+const i18n = createI18n({
+  locale: navigator.language,
+  fallbackLocale: 'en',
+  messages: { en, fr },
+  legacy: false,
+})
+app.use(i18n)
 
 app.use(createPinia())
 app.use(router)
